7276d8ac9aaf9734d7dea260adf78d058edb4ca4
starlin <starlinlq@gmail.com>
2021-12-15 15:42:57 -0600

    Initial commit

---
+++ src/main/java/com/teamProject/ManagmentSytem/ManagmentSytemApplication.java
@@ -0,0 +1,13 @@
+package com.teamProject.ManagmentSytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagmentSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagmentSytemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamProject/ManagmentSytem/ManagmentSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamProject.ManagmentSytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagmentSytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
